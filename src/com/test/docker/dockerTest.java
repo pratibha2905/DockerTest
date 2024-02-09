6f34e3f6a7ab2ee56680035123ff6cf5882ccfa2
ppandey200 <pratibha.pandey@dxc.com>
2024-02-09 18:32:34 +0530

    commit2

---
+++ src/com/test/docker/dockerTest.java
@@ -0,0 +1,10 @@
+package com.test.docker;
+
+public class dockerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("DOcker Hello WOrld");
+	}
+
+}
